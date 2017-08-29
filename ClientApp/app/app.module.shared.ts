9ae6d5dae0f336aa3ad1c7d24e9c3679a9947b55
WIMUT CHANKLANG <wimut.ch@gmail.com>
2017-08-29 17:22:18 +0700

    Add project files.

---
+++ ClientApp/app/app.module.shared.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { AccountListComponent } from './components/account/account-list/account-list.component';
+import { AccountSummaryComponent } from './components/account/account-summary/account-summary.component';
+import { AccountDetailComponent } from './components/account/account-detail/account-detail.component';
+import { FormatAccountNumberPipe } from './components/shared/format-account-number';
+import { AccountActivityComponent } from './components/account/acccount-activity/account-activity.component';
+import { AccountService } from './components/shared/account.service';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        AccountListComponent,
+        HeaderComponent,
+        AccountSummaryComponent,
+        AccountDetailComponent,
+        AccountActivityComponent,
+        FormatAccountNumberPipe
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'account', pathMatch: 'full' },
+            { path: 'account', component: AccountListComponent },
+            { path: 'detail/:id', component: AccountDetailComponent },
+            { path: '**', redirectTo: 'account' }
+        ])
+    ],
+    providers: [AccountService]
+})
+export class AppModuleShared {
+}
